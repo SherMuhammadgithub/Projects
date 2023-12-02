23b44cdf40de7820b32df6c254c965a735bba249
Munees Tariq <moontariq7867@gmail.com>
2023-12-02 17:46:41 +0500

    added Draw functionality and enhanced the win screen

--- C++/TicTacToe.cpp
+++ C++/TicTacToe.cpp
@@ -11,6 +11,8 @@ bool win(char [][8]);
 bool horizontalWin(char [][8]);
 bool verticalWin(char [][8]);
 bool diagonalWin(char [][8]);
+bool checkDraw(char [][8]);
+void doMove(char [][8], int, int, int &);
 int main() 
 {
     char board[7][8]=
@@ -32,46 +34,51 @@ int main()
     while(true)
     {
         if (win(board))
+            break;            // if game over then break
+        else if (checkDraw(board))
+        {
+            cout << "Draw!" << endl;
             break;
-        /// checking if the input is valid
+        }
         if (turn == 1)
         {
             cout << "Enter Your Move 'X' i.e(A0): ";
             x = getx();
             y = gety();
-            if (board[x*2+1][y*2+1] == ' ')
-            {
-                board[x*2+1][y*2+1] = 'X';
-                displayBoard(board);
-            }
-            else
-            {
-                cout << "Invalid Move!" << endl;
-                continue;
-            }
-            turn = 0;
+            doMove(board, x, y, turn);
         }
-        /// checking if the input is valid
         else if(turn == 0)
         {
             cout << "Enter Your Move 'O' i.e(A0): "; 
             x = getx();
             y = gety();
-            if (board[x*2+1][y*2+1] == ' ')
-            {
-                board[x*2+1][y*2+1] = 'O';
-                displayBoard(board);
-            }
-            else
-            {
-                cout << "Invalid Move!" << endl;
-                continue;
-            }
-            turn = 1;
+            doMove(board, x, y, turn);
         }
     }
     /// END ///
     return 0;
+}
+void doMove(char board[][8], int x, int y, int &turn)
+{
+    if (board[x*2+1][y*2+1] == ' ')
+    {
+        if (turn == 1)
+        {
+            board[x*2+1][y*2+1] = 'X';
+            turn = 0;
+        }
+        else
+        {
+            board[x*2+1][y*2+1] = 'O';
+            turn = 1;
+        }
+        displayBoard(board);
+    }
+    else
+    {
+        cout << "Invalid Move!" << endl;
+        turn = turn;
+    }
 }
 bool win(char board[][8])
 {
@@ -82,16 +89,28 @@ bool win(char board[][8])
         return true;
     return false;
 }
+bool checkDraw(char board[][8])
+{
+    for (int i = 1; i < BOARD_L; i+=2)
+    {
+        for (int j = 1; j < BOARD_W; j+=2)
+        {
+            if (board[i][j] == ' ')
+                return false;
+        }
+    }
+    return true;
+}
 bool diagonalWin(char board[][8])
 {
     if (board[1][1] == board[3][3] && board[3][3] == board[5][5] && board[1][1] != ' ')
     {
-        cout << board[1][1] << " Wins!" << endl;
+        cout << "Player \"" <<board[1][1] << "\" Wins!" << endl;
         return true;
     }
     else if (board[1][5] == board[3][3] && board[3][3] == board[5][1] && board[1][5] != ' ')
     {
-        cout << board[1][5] << " Wins!" << endl;
+        cout << "Player \"" <<board[1][5] << "\" Wins!" << endl;
         return true;
     }
     return false;
@@ -101,19 +120,19 @@ bool verticalWin(char board[][8])
     // checking  first column
     if (board[1][1] == board[3][1] && board[3][1] == board[5][1] && board[1][1] != ' ')
     {
-        cout << board[1][1] << " Wins!" << endl;
+        cout << "Player \"" <<board[1][1] << "\" Wins!" << endl;
         return true;
     }
     // checking  second column
     else if (board[1][3] == board[3][3] && board[3][3] == board[5][3] && board[1][3] != ' ')
     {
-        cout << board[1][3] << " Wins!" << endl;
+        cout << "Player \"" <<board[1][3] << "\" Wins!" << endl;
         return true;
     }
     // checking  third column
     else if (board[1][5] == board[3][5] && board[3][5] == board[5][5] && board[1][5] != ' ')
     {
-        cout << board[1][5] << " Wins!" << endl;
+        cout << "Player \"" <<board[1][5] << "\" Wins!" << endl;
         return true;
     }
     return false;
@@ -124,24 +143,11 @@ bool horizontalWin(char board[][8])
     {
         if (board[i][1] == board[i][3] && board[i][3] == board[i][5] && board[i][1] != ' ')
         {            
-            cout << board[i][1] << " Wins!" << endl;
+            cout << "Player \"" <<board[i][1] << "\" Wins!" << endl;
             return true;
         }
     }
     return false;
-}
-int getx()
-{
-    char temp = 0;              /// temp because it'll be converted to int
-    temp = getche();           /// getting char input
-    int x = temp - 'A';       /// converting char to int
-    return x;
-}
-int gety()
-{
-    int y = 0;
-    cin >> y;                 /// single digit input
-    return y;              /// returning the input
 }
 void displayBoard(char board[][8]) 
 {
@@ -165,4 +171,17 @@ void displayBoard(char board[][8])
         }
         cout << endl;
     }
+}
+int getx()
+{
+    char temp = 0;              /// temp because it'll be converted to int
+    temp = getche();           /// getting char input
+    int x = temp - 'A';       /// converting char to int
+    return x;
+}
+int gety()
+{
+    int y = 0;
+    cin >> y;                 /// single digit input
+    return y;              /// returning the input
 }
