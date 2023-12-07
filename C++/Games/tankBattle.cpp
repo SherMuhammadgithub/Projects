6c2157597f40f4e933d4c52a55ea693926315aaf
Munees Tariq <moontariq7867@gmail.com>
2023-12-07 09:33:31 +0500

    removing unessacary spaces

--- C++/Games/tankBattle.cpp
+++ C++/Games/tankBattle.cpp
@@ -1,64 +1,50 @@
 #include <iostream>
 #include <windows.h>
-
 using namespace std;
-
+//////////////////////////
 char getCharAtxy(short int x, short int y);
-void gotoxy(int x, int y);
-
-//
+void gotoxy(int x, int y);//
 int bonus1 = 0; 
-
 // enemy one
 void printEnemy1();
 void eraseEnemy1();
 void moveEnemy1();
-
 // enemy two
 void printEnemy2();
 void eraseEnemy2();
 void moveEnemy2();
-
 // enemy three
 void printEnemy3();
 void eraseEnemy3();
 void moveEnemy3();
-
 // player
 void erasePlayer();
 void printPlayer();
-
 // maze
 void printMaze();
-
 // header
 void header();
-
 //
 void score();
-
 // player movement
 void movePlayerLeft();
 void movePlayerRight();
 void movePlayerUp();
 void movePlayerDown();
-
-
 // enemy varaibles
 int e1X = 2, e1Y = 1;
 bool e1WallHit = false;
-
+///
 int e2X = 30, e2Y = 20;
 bool e2WallHit = false;
-
+///
 int e3X = 69, e3Y = 1;
 bool e3WallHit = false;
-
 // player variables
 int pX = 20, pY = 20;
-
 // main
-main(){
+int main()
+{
     system("cls");
     printMaze();
 
@@ -94,13 +80,11 @@ main(){
         cout << bonus1;
         
         Sleep(100);
-
-
     }
 }
-
 // enemy one
-void printEnemy1(){
+void printEnemy1()
+{
     gotoxy(e1X,e1Y);
     cout << "@    @" << endl;
     gotoxy(e1X,e1Y+1);
@@ -109,8 +93,8 @@ void printEnemy1(){
     cout << "@ EE @" << endl;
 
 }
-
-void eraseEnemy1(){
+void eraseEnemy1()
+{
     gotoxy(e1X,e1Y);
     cout << "      " << endl;
     gotoxy(e1X,e1Y+1);
@@ -118,148 +102,127 @@ void eraseEnemy1(){
     gotoxy(e1X,e1Y+2);
     cout << "      " << endl;
 }
-
-void moveEnemy1(){
-    eraseEnemy1();
-    
+void moveEnemy1()
+{
+    eraseEnemy1();    
     if (!e1WallHit){     
         e1X = e1X + 1;
         if(e1X == 42)
             e1WallHit = true;
     }
-
     else if(e1WallHit){ 
         e1X = e1X - 1;
         if(e1X < 3)
             e1WallHit = false;
     }
-
     printEnemy1();
-
 }
-
 // enemy two 
-void printEnemy2(){
+void printEnemy2()
+{
     gotoxy(e2X,e2Y);
     cout << "@ EE @" << endl;
     gotoxy(e2X,e2Y+1);
     cout << "@_EE_@" << endl;
     gotoxy(e2X,e2Y+2);
     cout << "@    @" << endl;
-
 }
-
-void eraseEnemy2(){
+void eraseEnemy2()
+{
     gotoxy(e2X,e2Y);
     cout << "      " << endl;
     gotoxy(e2X,e2Y+1);
     cout << "      " << endl;
     gotoxy(e2X,e2Y+2);
     cout << "      " << endl;
-
 }
-
 void moveEnemy2()
 {
     eraseEnemy2();
-
     if (!e2WallHit){        
         e2X = e2X + 1;
         e2Y = e2Y - 1;
         if(e2X == 43)
             e2WallHit = true;
-    }
-    
+    }    
     else if(e2WallHit){ 
         e2X = e2X - 1;
         e2Y = e2Y + 1;
         if(e2X == 30)
             e2WallHit = false;
     }
-    
     printEnemy2();
-
 }
-
 // enemy three
-void printEnemy3(){
+void printEnemy3()
+{
     gotoxy(e3X,e3Y);
     cout << "@-@-@" << endl;
     gotoxy(e3X,e3Y+1);
     cout << "333| " << endl;
     gotoxy(e3X,e3Y+2);
     cout << "@-@-@" << endl;
-
 }
-
-void eraseEnemy3(){
+void eraseEnemy3()
+{
     gotoxy(e3X,e3Y);
     cout << "     " << endl;
     gotoxy(e3X,e3Y+1);
     cout << "     " << endl;
     gotoxy(e3X,e3Y+2);
     cout << "     " << endl;
-
 }
-
-void moveEnemy3(){       
+void moveEnemy3()
+{       
     eraseEnemy3();
-
     if (!e3WallHit){        
         e3Y = e3Y + 1;
         if(e3Y == 20)
             e3WallHit = true;
     }
-
     else if(e3WallHit){
         e3Y = e3Y - 1;
         if(e3Y < 2)
             e3WallHit = false;
     }
-
     printEnemy3();
-
 }
-
 // player  
-void printPlayer(){
+void printPlayer()
+{
     gotoxy(pX,pY);
     cout << "@ || @" << endl;
     gotoxy(pX,pY+1);
     cout << "@_||_@" << endl;
     gotoxy(pX,pY+2);
-    cout << "@    @" << endl;
-    
+    cout << "@    @" << endl;    
 }
-
-void erasePlayer(){
+void erasePlayer()
+{
     gotoxy(pX,pY);
     cout << "      " << endl;
     gotoxy(pX,pY+1);
     cout << "      " << endl;
     gotoxy(pX,pY+2);
-    cout << "      " << endl;
-    
+    cout << "      " << endl;    
 }
-
 // player movements
-void movePlayerLeft(){
+void movePlayerLeft()
+{
     if (getCharAtxy(pX - 1, pY)== '*' || getCharAtxy(pX - 1, pY + 1) == '*' || getCharAtxy(pX - 1, pY + 2) == '*'){
         bonus1 += 1;
         erasePlayer();
         pX = pX - 1;
         printPlayer();
-    }    
-    
+    }        
     else if (getCharAtxy(pX - 1, pY) == ' ' && getCharAtxy(pX - 1, pY + 1) == ' ' && getCharAtxy(pX - 1, pY + 2) == ' '){
         erasePlayer();
         pX = pX - 1;
         printPlayer();
     }    
 }
-
-    
-void movePlayerRight(){
+void movePlayerRight()
+{
     if (getCharAtxy(pX + 6, pY)== '*' || getCharAtxy(pX + 6, pY + 1) == '*' || getCharAtxy(pX + 6, pY + 2) == '*'){
         bonus1 += 1;
         erasePlayer();
@@ -272,39 +235,36 @@ void movePlayerRight(){
         printPlayer();
     }
 }
-
-void movePlayerUp(){
+void movePlayerUp()
+{
     if (getCharAtxy(pX , pY - 1) == '*' || getCharAtxy(pX + 1 , pY - 1) == '*' || getCharAtxy(pX + 2, pY - 1) == '*' || getCharAtxy(pX + 3, pY - 1) == '*'|| getCharAtxy(pX + 4, pY - 1) == '*'|| getCharAtxy(pX + 5, pY - 1) == '*'){
         bonus1 +=1;
         erasePlayer();
         pY = pY - 1;
         printPlayer();
     } 
-
     else if(getCharAtxy(pX , pY - 1) == ' ' && getCharAtxy(pX + 1 , pY - 1) == ' ' && getCharAtxy(pX + 2, pY - 1) == ' '&& getCharAtxy(pX + 3, pY - 1) == ' '&& getCharAtxy(pX + 4, pY - 1) == ' '&& getCharAtxy(pX + 5, pY - 1) == ' '){
         erasePlayer();
         pY = pY - 1;
         printPlayer();
     }    
 }
-
-void movePlayerDown(){
+void movePlayerDown()
+{
    if (getCharAtxy(pX , pY + 3) == '*' || getCharAtxy(pX + 1 , pY + 3) == '*' || getCharAtxy(pX + 2, pY + 3) == '*' || getCharAtxy(pX + 3, pY + 3) == '*'|| getCharAtxy(pX + 4, pY + 3) == '*'|| getCharAtxy(pX + 5, pY + 3) == '*'){
         bonus1 +=1;
         erasePlayer();
         pY = pY + 1;
         printPlayer();
     }
-
     else if(getCharAtxy(pX , pY + 3) == ' ' && getCharAtxy(pX + 1, pY + 3) == ' ' && getCharAtxy(pX + 2, pY + 3) == ' ' && getCharAtxy(pX + 3, pY + 3) == ' ' && getCharAtxy(pX + 4, pY + 3) == ' ' && getCharAtxy(pX + 5, pY + 3) == ' '){    
         erasePlayer();
         pY = pY + 1;
         printPlayer();
     }
-
 }
-
-void printMaze(){
+void printMaze()
+{
 
     cout << "#############################################################################" << endl;
     cout << "##                                                                         ##" << endl;
@@ -331,13 +291,9 @@ void printMaze(){
     cout << "##                                                                         ##" << endl;
     cout << "##                                                                         ##" << endl;
     cout << "#############################################################################" << endl;
-
 }
-
-
-
-void header(){
-    
+void header()
+{    
     cout << endl;
     cout << endl;
     cout << endl;
@@ -356,12 +312,8 @@ void header(){
     cout<<R"(                              \ \  \|\__\_\  \ \  \ \  \ \  \\  \\|____|\  \  )"<<endl;
     cout<<R"(                               \ \____________\ \__\ \__\ \__\\ _\ ____\_\  \ )"<<endl;
     cout<<R"(                                \|____________|\|__|\|__|\|__|\|__|\_________\)"<<endl;
-    cout<<R"(                                                                  \|_________|)"<<endl;
-                                                                      
+    cout<<R"(                                                                  \|_________|)"<<endl;                                                                     
 }
-
-
-
 void score() 
 {
     int x = 80, y = 1; 
@@ -389,25 +341,10 @@ void score()
     
     gotoxy(x,y+7);
     cout << R"(    \|_________|                                       )" << endl;
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // functions for game
-char getCharAtxy(short int x, short int y){
+char getCharAtxy(short int x, short int y)
+{
     CHAR_INFO ci;
     COORD xy = {0, 0};
     SMALL_RECT rect = {x, y, x, y};
@@ -415,44 +352,11 @@ char getCharAtxy(short int x, short int y){
     coordBufSize.X = 1;
     coordBufSize.Y = 1;
     return ReadConsoleOutput(GetStdHandle(STD_OUTPUT_HANDLE), &ci, coordBufSize, xy, &rect) ? ci.Char.AsciiChar: ' ';
-
 }
-
-void gotoxy(int x, int y){
+void gotoxy(int x, int y)
+{
     COORD coordinates;
     coordinates.X = x;
     coordinates.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
-
 }
-
-
-
-
-////old headers
-
-// void score(){
-//     int x=80,y=1;
-
-//     gotoxy(x,y);
-//     cout << " #####     ####     ####     ######      #####"<< endl;
-//     gotoxy(x,y+1);
-//     cout << "#        ##       ##    ##   ##    ##   ##    "<< endl;
-//     gotoxy(x,y+2);
-//     cout << " ####    ##       ##    ##   ######     ####  "<< endl;
-//     gotoxy(x,y+3);
-//     cout << "     #   ##       ##    ##   ##   ##    ##    "<< endl;
-//     gotoxy(x,y+4);
-//     cout << "#####      ####     ####     ##    ##    #####"<< endl;
-
-// }
-
-// void header(){
-//     cout << endl;
-//     cout << "######     ###     ###   ##   ##   ##      ##      ##     ###     ######      #####  " << endl;
-//     cout << "  ##     ##   ##   ## #  ##   ##  ##       ##      ##   ##   ##   ##    ##   ##      " << endl;
-//     cout << "  ##     ### ###   ##  # ##   ####         ##  ##  ##   ### ###   ######      #####  " << endl;
-//     cout << "  ##     ##   ##   ##   ###   ##  ##       ## #  # ##   ##   ##   ##   ##         ## " << endl;
-//     cout << "  ##     ##   ##   ##    ##   ##   ##      ###    ###   ##   ##   ##    ##    #####  " << endl;
-
-// }
